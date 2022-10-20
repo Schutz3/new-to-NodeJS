c8bd9c9f40a36ca2c044860bda9bbadfa4fa5dc4
Farhan Dwi <02.farhan.dwi@gmail.com>
2022-10-20 13:57:37 +0700

    Learn Raw Node JS

--- Journey/D001/app.js
+++ Journey/D001/app.js
@@ -2,12 +2,34 @@ const http = require('http');
 const fs = require('fs');
  
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Hello W</title></head>');
-    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
+    res.write('<body><h1>Hello From Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
 
