9dd20851a376dbfd977f41eb98b16c729983567b
Farhan Dwi <02.farhan.dwi@gmail.com>
2022-10-20 11:50:42 +0700

    Playing With Destructuring

--- Journey/D001/03PlayWith.js
+++ Journey/D001/03PlayWith.js
@@ -1,18 +1,18 @@
 //Global Variables
-let name = 'Farhan';
+let nama = 'Farhan';
 //Const = Var Tetap
-const midname = 'Dwi';
+const midnama = 'Dwi';
 //Let = Var yang bisa diubah
 let umur = 20;
 let DOB = "2002-10-04";
 let punyaKTP = true;
 
-//Output nama / name
-console.log(name +" "+ midname +"\n"+ umur +"\n"+ DOB);
+//Output nama / nama
+console.log(nama +" "+ midnama +"\n"+ umur +"\n"+ DOB);
 
 console.log("\n=====================================\n");
 
-function cekUser(userName, userAge, userDOB, userKTP){
+function cekUser(usernama, userAge, userDOB, userKTP){
     //Local Variables
     let KTP;
     if (userKTP == true){
@@ -21,21 +21,21 @@ function cekUser(userName, userAge, userDOB, userKTP){
         KTP = "tidak punya";
     }
 
-    return "Nama saya adalah " + userName + "\ndan saya berumur " + userAge + " tahun.\nSaya lahir pada tanggal " + userDOB + "\ndan saya " + KTP + " KTP. ("+ userKTP +")";
+    return "Nama saya adalah " + usernama + "\ndan saya berumur " + userAge + " tahun.\nSaya lahir pada tanggal " + userDOB + "\ndan saya " + KTP + " KTP. ("+ userKTP +")";
 }
 
-console.log(cekUser(name, umur, DOB, punyaKTP));
+console.log(cekUser(nama, umur, DOB, punyaKTP));
 
 console.log("\n=====================================\n");
 
 punyaKTP = false;
 
-console.log(cekUser(name, umur, DOB, punyaKTP));
+console.log(cekUser(nama, umur, DOB, punyaKTP));
 
 console.log("\n=====================================\n");
 
 //Arrow Function
-const cekUserV2 = (userName, userAge, userDOB, userKTP) => {
+const cekUserV2 = (usernama, userAge, userDOB, userKTP) => {
     //Local Variables
     let KTP;
     if (userKTP == true){
@@ -44,7 +44,7 @@ const cekUserV2 = (userName, userAge, userDOB, userKTP) => {
         KTP = "tidak punya";
     }
 
-    return "Nama saya adalah " + userName + "\ndan saya berumur " + userAge + " tahun.\nSaya lahir pada tanggal " + userDOB + "\ndan saya " + KTP + " KTP. ("+ userKTP +")";
+    return "Nama saya adalah " + usernama + "\ndan saya berumur " + userAge + " tahun.\nSaya lahir pada tanggal " + userDOB + "\ndan saya " + KTP + " KTP. ("+ userKTP +")";
 }
 
 const sum = (a, b) => a + b;
@@ -54,7 +54,7 @@ const substract = (a, b) => a - b;
 const addOne = (a) => a + 1;
 const randFunc = () => Math.random();
 
-console.log(cekUserV2(name, umur, DOB, punyaKTP));  
+console.log(cekUserV2(nama, umur, DOB, punyaKTP));  
 console.log(addOne(2));
 console.log(sum(6, 9));
 console.log(addOne(sum(6, 9)));
@@ -64,18 +64,18 @@ console.log(randFunc());
 console.log("\n=====================================\n");
 //Playing With Object
 const person = {
-    name: 'Farhan',
+    nama: 'Farhan',
     age: 20,
     DOB: '2002-10-04',
 
     // 1st Method
     // greeting: function() {
-    //     cconsole.log("Nama saya adalah " + this.name + "\ndan saya berumur " + this.age + " tahun.\nSaya lahir pada tanggal " + this.DOB);
+    //     cconsole.log("Nama saya adalah " + this.nama + "\ndan saya berumur " + this.age + " tahun.\nSaya lahir pada tanggal " + this.DOB);
     // }
 
     // 2nd Method
     greeting() {
-        console.log("Nama saya adalah " + this.name + "\ndan saya berumur " + this.age + " tahun.\nSaya lahir pada tanggal " + this.DOB);
+        console.log("Nama saya adalah " + this.nama + "\ndan saya berumur " + this.age + " tahun.\nSaya lahir pada tanggal " + this.DOB);
     }
     
 
@@ -84,7 +84,7 @@ const person = {
 
 console.log(person);
 
-console.log(person.name);
+console.log(person.nama);
 
 person.greeting();
 
@@ -128,12 +128,30 @@ const toArray = (...args) => {
 
 console.log(toArray( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-console.log(toArray( name, midname, umur,));
+console.log(toArray( nama, midnama, umur,));
 
 console.log("\n=====================================\n");
+//Playing With Destructuring
 
+const printnama = (person) => {
+    console.log(person.nama);
+}
 
+const printnamaV2 = ({DOB}) => {
+    console.log(DOB);
+}
 
+const { name, age } = person;
+console.log(nama, age);
+
+printnama(person);
+printnamaV2(person);
+
+const [Book1, Book2] = judulBuku;
+console.log(Book1, Book2);
+
+console.log("\n=====================================\n");
+//Playing With Object Copying
 
 
 
