0107b65578212a3d5348b0b56e8aa0f5773ffe48
Farhan Dwi <02.farhan.dwi@gmail.com>
2022-10-21 16:39:24 +0700

    learn Debugging

--- Journey/D002/routes.js
+++ Journey/D002/routes.js
@@ -17,7 +17,7 @@ const reqHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
+            const message = parsedBody.split('=')[0];
             fs.writeFile('message.txt', message, err => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
