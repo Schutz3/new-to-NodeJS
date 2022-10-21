c013b4fbf650ababe0f1e9a1e7ca7ef8650ac54f
Farhan Dwi <02.farhan.dwi@gmail.com>
2022-10-21 16:27:59 +0700

    learn nodemon

--- Journey/D002/app.js
+++ Journey/D002/app.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 const server = http.createServer(routes)
 
 
-
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
     console.log(routes.someText);
