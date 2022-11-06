58caaea2b347646a34056416d82583861773b116
Farhan Dwi <02.farhan.dwi@gmail.com>
2022-11-07 06:28:45 +0700

    Handling Routes

--- Journey/D002/app.js
+++ Journey/D002/app.js
@@ -2,13 +2,18 @@ const http = require('http');
 const express = require('express');
 const app = express();  
 
+app.use((req, res, next) => {
+    console.log('This always executes!');
+    next();
+});
+
 app.use('/gwk', (req, res, next) => {
     console.log('In the /gwk middleware');
     res.send('<h1>Hello /gwk!</h1>');
 //    next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>');
 });
